05e7e3411168c49d6502a0370f2a3db2d7c29748
Alejandra Urcelay <alejandraurcelay@gmail.com>
2018-11-21 17:25:00 -0800

    Added some other features.

--- bcl/app.R
+++ bcl/app.R
@@ -7,10 +7,11 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
   titlePanel("BC Liquor Store prices"),
-  img(src = "BClogo.png", height = 52.25, width = 500, align = "right"),
+  img(src = "BClogo.png", height = 52.25, width = 500, align = "right"), #add logo
   sidebarLayout(
     sidebarPanel(
       sliderInput("priceInput", "Price", 0, 100, c(0, 35), pre = "$"),
+      #allow to select more than one type
       checkboxGroupInput("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "BEER",
@@ -19,9 +20,18 @@ ui <- fluidPage(
       uiOutput("countryOutput")
     ),
     mainPanel(
+      textOutput("optionsText"),#outputs options found with user selection
+      #customize text output
+      tags$head(tags$style("#optionsText{color: #DC143C;
+                                 font-size: 25px;
+                                 font-style: bold;
+                                 }"
+      )
+    ),
+      downloadButton("download", "Download options"), #download the options found
       plotOutput("coolplot"),
       br(), br(),
-      DT::dataTableOutput("results")
+      DT::dataTableOutput("results") #make table interactive
       )
     )
   )
@@ -37,7 +47,10 @@ server <- function(input, output) {
   filtered <- reactive({
     if (is.null(input$countryInput)) {
       return(NULL)
-    }    
+    }
+    if(is.null(input$typeInput)){
+      return(NULL) #avoid an error when no type is selected
+    }
     
     bcl %>%
       filter(Price >= input$priceInput[1],
@@ -49,15 +62,33 @@ server <- function(input, output) {
   
   output$coolplot <- renderPlot({
     if (is.null(filtered())) {
-      return()
+      return(NULL)
     }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+    ggplot(filtered(), aes(Alcohol_Content, fill = Type)) +
+      geom_histogram() +
+      labs(x = "Alcohol Content (%)")
   })
   
   output$results <- DT::renderDataTable({
     filtered()
   })
+  
+  output$optionsText <- renderText({
+    optionsNumber <- nrow(filtered())
+    if (is.null(optionsNumber)) {
+      optionsNumber <- 0
+    }
+    paste("We found", optionsNumber, "options to get you tipsy")
+  })
+  
+  output$download <- downloadHandler(
+    filename = function() {
+      "bcl-options.csv"
+    },
+    content = function(con) {
+      write.csv(filtered(), con)
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
